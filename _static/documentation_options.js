42f0f4258135ceda5eff6c4f03db7abd5cf12dba
christopherpickering <christopherpickering@users.noreply.github.com>
2021-12-02 08:25:54 +0000

    deploy: 176ddb231fc74d60b3f3f860e9a035f63d862c85

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,7 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '0.6.7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
+    BUILDER: 'dirhtml',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
